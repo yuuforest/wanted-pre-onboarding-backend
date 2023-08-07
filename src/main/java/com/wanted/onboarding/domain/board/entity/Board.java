a78d3a3af70da01ebd5c643caf9847ce78ec04e9
yurrim <yurimkang20@gmail.com>
2023-08-07 14:43:47 +0900

    [Feat] entity 생성
    Date, Board, Member, Token entity 생성

---
+++ src/main/java/com/wanted/onboarding/domain/board/entity/Board.java
@@ -0,0 +1,29 @@
+package com.wanted.onboarding.domain.board.entity;
+
+import com.wanted.onboarding.common.BaseDateEntity;
+import com.wanted.onboarding.domain.member.entity.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseDateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long boardSeq;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name="memberSeq")
+    private Member member;
+
+}
---
+++ src/main/java/com/wanted/onboarding/domain/member/entity/Token.java
@@ -0,0 +1,20 @@
+package com.wanted.onboarding.domain.member.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long tokenSeq;
+
+    @Column(nullable = false)
+    private String refreshToken;
+}
