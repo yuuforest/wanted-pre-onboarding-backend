9edb11ac5665acc370a4ce4e2a77d4bdde4cd874
yurrim <yurimkang20@gmail.com>
2023-08-08 18:20:00 +0900

    [Feat] 회원가입 비밀번호 암호화 기능 추가

--- src/main/java/com/wanted/onboarding/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/wanted/onboarding/domain/member/service/MemberServiceImpl.java
@@ -6,6 +6,7 @@ import com.wanted.onboarding.domain.member.repository.MemberRepository;
 import com.wanted.onboarding.error.code.MemberErrorCode;
 import com.wanted.onboarding.error.exception.ErrorException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void register(RegisterRequestDto requestDto) {
@@ -20,15 +22,14 @@ public class MemberServiceImpl implements MemberService {
         String email = requestDto.getEmail();
         String password = requestDto.getPassword();
 
-        // 아이디 유효 검사
+        // 아이디 및 비밀번호 유효 검사
         if (!email.contains("@")) throw new ErrorException(MemberErrorCode.ID_MISSING_SYMBOL);
 
-        // 비밀번호 유효 검사 및 암호화 처리
         if (password.length() < 8) throw new ErrorException(MemberErrorCode.PASSWORD_SHORT_LENGTH);
 
         Member member = Member.builder()
                 .id(email)
-                .password(password)
+                .password(passwordEncoder.encode(password))
                 .build();
 
         memberRepository.save(member);
